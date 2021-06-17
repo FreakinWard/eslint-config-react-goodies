e926d533bcfe24b86ef599e578a6a0b8d7ed2a0a
aaronward <aaron.l.ward@gmail.com>
2021-06-17 09:52:55 -0400

    rebase main pulled out eslint config, tested
    # Conflicts:
    #	README.md

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,30 +3,27 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2020, // Use the latest ecmascript standard
     sourceType: 'module', // Allows using import/export statements
-    ecmaFeatures: {
-      jsx: true, // Enable JSX since we're using React
-    },
-  },
-  settings: {
-    react: {
-      version: 'detect', // Automatically detect the react version
-    },
   },
   env: {
+    browser: true, // Enables browser globals like window and document
+    amd: true, // Enables require() and define() as global variables as per the amd spec.
     node: true, // Enables Node.js global variables and Node.js scoping.
     jest: true,
   },
   plugins: ['simple-import-sort', 'prettier', 'import'],
   extends: [
     'eslint:recommended',
-    './config/rules/react',
-    './config/rules/jest',
+    'plugin:jest/style',
     'plugin:prettier/recommended', // Make this the last element so prettier config overrides other formatting rules
   ],
   rules: {
-    'import/prefer-default-export': 'error',
     'prettier/prettier': 'error', // Use our prettier.config.js file as source
     'simple-import-sort/imports': 'error',
     'simple-import-sort/exports': 'error',
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
   },
 };
